42d65cd399f791acb7614ad705438f45c95eedd8
Chbmleao <caikleao@gmail.com>
2024-01-25 09:58:51 -0300

    style: change project font

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
 import { createGlobalStyle } from "styled-components";
+import { Helmet } from "react-helmet";
 
 const GlobalStyles = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
+    font-family: 'Open Sans', sans-serif;
   }
 `;
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+        />
+      </Helmet>
       <GlobalStyles />
       <Component {...pageProps} />
     </div>
