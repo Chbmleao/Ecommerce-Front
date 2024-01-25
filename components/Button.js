acd6c7338ae36342d70e0345fe010f73e73fd006
Chbmleao <caikleao@gmail.com>
2024-01-25 18:02:38 -0300

    fix: removing warnings

--- components/Button.js
+++ components/Button.js
@@ -14,19 +14,19 @@ export const ButtonStyle = css`
     height: 1.2em;
     margin-right: 5px;
   }
-  ${props => props.black && `
+  ${props => props.black && css`
     background-color: #222;
   `}
-  ${props => props.outline &&`
+  ${props => props.outline && css`
     background-color: transparent;
     border: 1px solid #222;
     color: #222;
   `}
-  ${props => props.primary && `
+  ${props => props.primary && css`
     background-color: #5542F6;
     border: 1px solid #5542F6;
   `}
-  ${props => props.size === "l" && `
+  ${props => props.size === "l" && css`
     font-size: 1.2rem;
     padding: 10px 20px;
     svg {
