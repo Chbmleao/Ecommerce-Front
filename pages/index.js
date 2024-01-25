c5c05ff93328ba42b7bf995f977217130d48aa28
Chbmleao <caikleao@gmail.com>
2024-01-25 17:54:22 -0300

    feat: get featured product from database

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
+import { Product } from "@/models/Product";
+import { mongooseConnect } from "@/lib/mongoose";
 
-export default function HomePage() {
+export default function HomePage({product}) {
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product}/>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "65b2bd8235656e3919b53a21";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: {
+      product: JSON.parse(JSON.stringify(product)),
+    }
+  };
 }
