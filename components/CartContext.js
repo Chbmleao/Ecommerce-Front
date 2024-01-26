ceb954636ad7f356743decfbd6a378175aad0778
Chbmleao <caikleao@gmail.com>
2024-01-26 13:23:30 -0300

    feat: add cart to localstorage

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,9 +1,21 @@
-import { createContext, useState, useMemo } from "react";
+import { createContext, useState, useMemo, useEffect } from "react";
 
 export const CartContext = createContext({});
 
 export function CartContextProvider({children}) {
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
   const [cartProducts, setCartProducts] = useState([]);
+
+  useEffect(() => {
+    if(cartProducts?.length > 0) {
+      ls?.setItem("cartProducts", JSON.stringify(cartProducts));
+    }
+  }, [cartProducts]);
+  useEffect(() => {
+    if (ls?.getItem("cartProducts")) {
+      setCartProducts(JSON.parse(ls.getItem("cartProducts")));
+    }
+  }, []);
   
   function addProductToCart(productId) {
     setCartProducts(prev => [...prev, productId]);
