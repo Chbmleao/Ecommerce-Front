9b8241d1f175f95b978ba28345c828b7f6810c8c
Chbmleao <caikleao@gmail.com>
2024-02-02 15:32:52 -0300

    feat: add responsiveness to header component

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styled from "styled-components";
 import Center from "@/components/Center";
 import { CartContext } from "@/components/CartContext";
+import BarsIcon from "./icons/Bars";
 
 const StyledHeader = styled.header`
   background-color: #222;
@@ -12,6 +13,8 @@ const StyledHeader = styled.header`
 const Logo = styled(Link)`
   color: #fff;
   text-decoration: none;
+  position: relative;
+  z-index: 3;
 `
 
 const Wrapper = styled.div`
@@ -21,17 +24,53 @@ const Wrapper = styled.div`
 `;
 
 const StyledNav = styled.nav`
-  display: flex;
+  ${props => props.mobileNavActive ? `
+    display: block;
+  ` : `
+    display: none;
+  `}
   gap: 20px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 70px 20px 20px;
+  background-color: #222;
+  @media screen and (min-width: 768px) {  
+    display: flex;
+    position: static;
+    padding: 0;
+  }
 `;
 
 const NavLink = styled(Link)`
+  display: block;
   color: ${(props) => (props.is_current_page ? "white" : "#aaa")};
   text-decoration: none;
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding: 0;
+  }
+`;
+
+const NavButton = styled.button`
+  background-color: transparent;
+  width: 35px;
+  height: 35px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  position: relative;
+  z-index: 3;
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
 `;
 
 export default function Header() {
   const {getCartLength} = useContext(CartContext);
+  const [mobileNavActive, setMobileNavActive] = useState(false);
   const router = useRouter();
 
   return (
@@ -39,7 +78,7 @@ export default function Header() {
       <Center>
         <Wrapper>
           <Logo href={"/"}>Ecommerce</Logo>
-          <StyledNav>
+          <StyledNav mobileNavActive={mobileNavActive}>
             <NavLink 
               href={"/"} 
               is_current_page={(router.pathname === "/").toString()}
@@ -71,6 +110,9 @@ export default function Header() {
               Cart ({getCartLength()})
             </NavLink>
           </StyledNav>
+          <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
+            <BarsIcon />
+          </NavButton>
         </Wrapper>
       </Center>
     </StyledHeader>
